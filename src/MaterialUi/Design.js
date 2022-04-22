eca5c98c0121ef69533d5b294a90e4e123a82b2a
suprava padhi <supravapadhi92@gmail.com>
2022-04-22 21:06:24 +0530

    cards design

--- src/MaterialUi/Design.js
+++ src/MaterialUi/Design.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Button, Grid } from '@mui/material';
+import { Button, Grid, Card, CardContent, CardActions } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 function Design() {
   return (
     <div>
       {/* <Button variant='contained' color="error" size="medium" startIcon={<AccountCircleIcon/>}>LOGIN</Button> */}
 
-      <Grid container style={{ border: "3px solid red" }}>
+      {/* <Grid container style={{ border: "3px solid red" }}>
 
         <Grid item xs={12} md={6} lg={4} style={{ border: "2px solid green" }}>
           Item widths are set in percentages, so they're always fluid and sized relative to their parent element.
@@ -21,6 +21,63 @@ function Design() {
           Items have padding to create the spacing between individual items.
         </Grid>
 
+      </Grid> */}
+
+
+      {/* card design */}
+      <Grid container spacing={4}>
+        <Grid item xs={12} md={6} lg={3}>
+          <Card>
+            <CardContent>
+            <h2>Title</h2>
+              Item widths are set in percentages, so they're always fluid and sized relative to their parent element.
+              Items have padding to create the spacing between individual items.
+            </CardContent>
+            <CardActions>
+              <Button size="small" color='secondary' variant='contained'>READ MORE</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* 2nd card */}
+        <Grid item xs={12} md={6} lg={3}>
+          <Card>
+            <CardContent>
+            <h2>Title-2</h2>
+              Item widths are set in percentages, so they're always fluid and sized relative to their parent element.
+              Items have padding to create the spacing between individual items.
+            </CardContent>
+            <CardActions>
+              <Button size="small" color='secondary' variant='contained'>READ MORE</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        {/* 3rd card */}
+        <Grid item xs={12} md={6} lg={3}>
+          <Card>
+            <CardContent>
+            <h2>Title-3</h2>
+              Item widths are set in percentages, so they're always fluid and sized relative to their parent element.
+              Items have padding to create the spacing between individual items.
+            </CardContent>
+            <CardActions>
+              <Button size="small" color='secondary' variant='contained'>READ MORE</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+
+        {/* 4th card */}
+        <Grid item xs={12} md={6} lg={3}>
+          <Card>
+            <CardContent>
+            <h2>Title-4</h2>
+              Item widths are set in percentages, so they're always fluid and sized relative to their parent element.
+              Items have padding to create the spacing between individual items.
+            </CardContent>
+            <CardActions>
+              <Button size="small" color='secondary' variant='contained'>READ MORE</Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
 
     </div>
